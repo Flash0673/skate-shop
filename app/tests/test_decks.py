34b62fb9aba6f9462c5f9e45bdc1bf3b6eb9ecaf
Никита Рыбаковский <narybakovskiy@edu.hse.ru>
2023-12-16 03:33:19 +0300

    tests added

---
+++ app/tests/test_decks.py
@@ -0,0 +1,55 @@
+import json
+
+import pytest
+
+from app.db import crud
+
+
+def test_create_deck(test_app, monkeypatch):
+    test_data = {
+        "description": "test",
+        "title": "test",
+        "id": 0,
+        "price": 6000.0,
+        "size": 8.25,
+        "title": "test_deck",
+    }
+
+    def mock_post(db, deck):
+        return test_data
+
+    monkeypatch.setattr(crud, "create_deck", mock_post)
+
+    response = test_app.post("/decks", content=json.dumps(test_data),)
+    assert response.status_code == 200
+    assert response.json() == test_data
+
+
+def test_create_deck_invalid_json(test_app):
+    response = test_app.post("/decks", content=json.dumps({"description": "test"}))
+    assert response.status_code == 422
+
+    response = test_app.post(
+        "/decks", content=json.dumps({"description": 8.25, "size": "test"})
+    )
+    assert response.status_code == 422
+
+
+def test_read_deck(test_app, monkeypatch):
+    test_data = {
+        "description": "test",
+        "title": "test",
+        "id": 0,
+        "price": 6000.0,
+        "size": 8.25,
+        "title": "test_deck",
+    }
+
+    def mock_get(db, deck_id):
+        return test_data
+
+    monkeypatch.setattr(crud, "get_deck", mock_get)
+
+    response = test_app.get("/decks/0")
+    assert response.status_code == 200
+    assert response.json() == test_data
---
+++ app/tests/test_trucks.py
@@ -0,0 +1,59 @@
+import json
+
+import pytest
+
+from app.db import crud
+
+
+def test_create_trucks(test_app, monkeypatch):
+    test_data = {
+        "description": "test",
+        "title": "test",
+        "id": 0,
+        "price": 6000.0,
+        "size": 5.5,
+        "title": "test_truck",
+        "quantity": 2,
+        "has_bushing": True,
+    }
+
+    def mock_post(db, truck):
+        return test_data
+
+    monkeypatch.setattr(crud, "create_truck", mock_post)
+
+    response = test_app.post("/trucks", content=json.dumps(test_data),)
+    assert response.status_code == 200
+    assert response.json() == test_data
+
+
+def test_create_trucks_invalid_json(test_app):
+    response = test_app.post("/trucks", content=json.dumps({"description": "test"}))
+    assert response.status_code == 422
+
+    response = test_app.post(
+        "/trucks", content=json.dumps({"has_bushing": 5, "size": "test"})
+    )
+    assert response.status_code == 422
+
+
+def test_read_trucks(test_app, monkeypatch):
+    test_data = {
+        "description": "test",
+        "title": "test",
+        "id": 0,
+        "price": 6000.0,
+        "size": 5.5,
+        "title": "test_truck",
+        "quantity": 2,
+        "has_bushing": True,
+    }
+
+    def mock_get(db, truck_id):
+        return test_data
+
+    monkeypatch.setattr(crud, "get_truck", mock_get)
+
+    response = test_app.get("/trucks/0")
+    assert response.status_code == 200
+    assert response.json() == test_data
